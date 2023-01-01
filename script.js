c3c33386bfbdd558980f7cca8fe4940ac98ca98e
nichoh <101162836+nichoh@users.noreply.github.com>
2023-01-01 20:16:17 +0700

    reference to javascript file

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
-function setupTabs (){
+function setupTabs(){
     document.querySelectorAll(".type-button").forEach(button =>{
-        button.addEventListener("click", ()=>{
-            const typeButton = button.parentElement;
-            const typeContent= typeButton.parentElement;
-            const typeNumber= button.dataset.forMacbooktype;
-            const activateTab= typeContent.querySelector(`.type-content[data-typeContent="${typeNumber}"]`);
+        button.addEventListener("click", () =>{
+            const typeBar = button.parentElement;
+            const barContainer = typeBar.parentElement;
+            const typeNumber = button.dataset.forTab;
+            const typeToActivate = barContainer.querySelector(`.type-content[data-tab="${typeNumber}"]`);
+
+            typeBar.querySelectorAll(".type-button").forEach(button => {
+                button.classList.remove("type-button--active");
+            });
+
+            barContainer.querySelectorAll(".type-content").forEach(tab => {
+                tab.classList.remove("type-content--active");
+            });
+            
+            button.classList.add("type-button--active");
+            typeToActivate.classList.add("type-content--active");
+
+            console.log(typeBar)
+            console.log(barContainer)
+            console.log(typeNumber)
+            console.log(typeToActivate)
         });
     });
 }
 
-document.addEventListener("DOMContentLoaded", ()=>{
+document.addEventListener("DOMContentLoaded", () => {
     setupTabs();
+
+    document.querySelectorAll('.tabs').forEach(barContainer=>{
+        barContainer.querySelector('.macbookType-Container .type-button').click();
+    });
 });
