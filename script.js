d7982bcd2d2126b3a8224d0ecdaf0edc25158957
nichoh <101162836+nichoh@users.noreply.github.com>
2023-01-10 23:04:29 +0700

    Update
    add to cart moved to checkout page, add form to checkout page

--- script.js
+++ script.js
@@ -42,8 +42,6 @@ hambMenu.addEventListener('click', () => {
 
 //   Checkout
 
-// var cartItem = document.getElementsByClassName('cart-box')
-
 if(document.readyState=='loading'){
     document.addEventListener('DOMContentLoaded', ready)
 }else{
@@ -63,50 +61,7 @@ function ready(){
         var input=quantityInputs[i];
         input.addEventListener("change", changedquantity);
     }
-
-    var addtoCart = document.getElementsByClassName('btn-AddToCart');
-        for(var i=0; i<addtoCart.length; i++){
-            var addItem=addtoCart[i];
-            addItem.addEventListener("click", AddingItem);
-        }
     }
-
-function AddingItem(event){
-    var button = event.target;
-    var StoreItem = button.parentElement.parentElement;
-    var macModel = StoreItem.getElementsByClassName('mac-name')[0].innerText;
-    var macPrice = StoreItem.getElementsByClassName('mac-price')[0].innerText;
-    var macImg = StoreItem.getElementsByClassName('mac-pic')[0].src;
-    AddItemToCart(macModel, macPrice, macImg);
-    updateTotal();
-}
-
-function AddItemToCart(macModel, macPrice, macImg){
-    var cartShopBox = document.createElement("div");
-    cartShopBox=classList.add('cart-box')
-    var cartProduct= document.getElementsByClassName('cart-box')[0]
-    var cartProductName=cartProduct.getElementsByClassName('cart-detail-name');
-    for(var i=0; i<cartProductName.length; i++){
-        if(cartProductName[i].innerText==macModel){
-            alert("This item is already in your cart");
-            return;
-        }
-    }
-    var cartBoxContent=`
-    <img class="cart-image" src="Asset/Air-M1.png" alt="">
-    <div class="cart-detail-box">
-        <div class="cart-detail-name">Macbook-Air M1</div>
-        <div class="cart-detail-price">$999</div>
-        <input type="number" value="1" class="cart-detail-quantity">
-    </div>
-
-    <div class="cart-remove"><button class="remove-button">Trash</button></div>`;
-
-    cartShopBox.innerHTML=cartBoxContent;
-    cartProduct.append(cartBoxContent);
-    cartBoxContent.getElementsByClassName("cart-remove")[0].addEventListener("click", removeCartItem);
-    cartBoxContent.getElementsByClassName("cart-detail-quantity")[0].addEventListener("change", changedquantity);
-}
 
 function removeCartItem(event){
     var buttonClicked=event.target;
@@ -117,7 +72,7 @@ function removeCartItem(event){
 function changedquantity(event){
     var input=event.target
     if(isNaN(input.value) || input.value <=0) {
-        input.value=1;
+        input.value=0;
     }
     updateTotal()
 }
